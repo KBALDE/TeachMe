e84386147342b49eeea3ac3ed84bfa01bbdcba7f
kbalde <kindi.balde@gmail.com>
2017-07-31 21:02:30 +0200

    First Commit
    First Commit

---
+++ utils.py
@@ -0,0 +1,10 @@
+import os 
+import system
+
+def pause():
+    programPause=raw_input("Press the <Enter> key to continue")
+
+# this is the more rational way to do pause
+
+# adding a new line of comment
+
